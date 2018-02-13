0655c1bcc90082a47d0cb6ddba5026141ccd4560
KyeongSoo Yoo <>
2018-02-14 02:50:23 +0900

    몇몇 폰트 관련 수정
    Signed-off-by: KyeongSoo Yoo <>

--- src/components/project/project.vue
+++ src/components/project/project.vue
@@ -12,7 +12,7 @@
                     <a target='_blank' href='https://itunes.apple.com/kr/app/%EC%99%80%EC%9D%B4%EB%93%9C%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%99%80%EC%9D%B4%ED%8C%8C%EC%9D%B4%EB%8F%84%EC%8B%9C%EB%9D%BD/id1162744579?mt=8' class='align-text-top badge badge-primary'>link</a>
                 </h5>
                 <p class='w-100 text-left'>
-                    (주)와이드모바일의 모바일 사이트의 리뉴얼과 iOS에서의 하이브리드 어플리케이션 개발업무를 17년 4월부터 9월까지 약 6개월 가량 진행하였습니다. ASP.NET MVC3, AngularJS, jQuery등으로 이루어진 웹페이지와 Objective C기반으로 CocoaPod의 라이브러리들을 이용해 하이브리드 어플리케이션 개발 및 배포가 이루어졌습니다.
+                    (주)와이드모바일의 모바일 사이트의 리뉴얼 보조 업무와 iOS에서의 하이브리드 어플리케이션 개발업무를 <span class='text-info font-italic '>17년 4월부터 9월까지 약 6개월</span> 가량 진행하였습니다. <span class='text-info font-italic '>ASP.NET MVC3, AngularJS, jQuery</span>등으로 이루어진 웹페이지와 <span class='text-info font-italic '>Objective C기반으로 CocoaPod의 라이브러리들을 이용해 하이브리드 어플리케이션 개발 및 배포</span>가 이루어졌습니다.
                 </p>
             </div>
             <div class='project-content'>
@@ -21,7 +21,7 @@
                     <a target='_blank' href='https://www.wifidosirak.com/' class='align-text-top badge badge-primary'>link</a>
                 </h5>
                 <p class='w-100 text-left'>
-                    (주)와이드모바일의 주요사이트들의 유지보수를 보조를 17년 3월 입사 후 10월 퇴사 전까지 진행하였습니다. ASP.NET 4.0과 MSSQL을 사용했으며 주로 jQuery와 바닐라 Javascript를 사용하였습니다. jQueryUI, SwiperJS등 다양한 자바스크립트 라이브러리들을 사용하였습니다.
+                    (주)와이드모바일의 주요사이트들의 유지보수를 보조를 17년 3월 입사 후 10월 퇴사 전까지 <span class='text-info font-italic'>약 8개월 가량 진행</span>하였습니다. <span class='text-info font-italic'>ASP.NET 4.0과 MSSQL</span>을 사용했으며 주로 <span class='text-info font-italic'>jQuery와 바닐라 Javascript를 사용</span>하였습니다. jQueryUI, SwiperJS등 다양한 자바스크립트 라이브러리들을 사용하였습니다.
                 </p>
             </div>
             <div class='project-content'>
@@ -30,7 +30,7 @@
                     <a target='_blank' href='https://itunes.apple.com/kr/app/%EC%9C%A0%EB%B9%84%EC%8A%A4-%EC%97%85-%EB%8B%A4%EC%9A%B4/id593774586?mt=8' class='align-text-top badge badge-primary'>link</a>
                 </h5>
                 <p class='w-100 text-left'>
-                    (주)유씨알엠의 고객관리 서비스 U-bis(유비스)의 주소록 업/다운로드 어플리케이션 개발을 13년도 3개월 가량 진행하였습니다. 개발 및 배포를 담당하였고, Objective C가 사용되었습니다. 
+                    (주)유씨알엠의 고객관리 서비스 U-bis(유비스)의 주소록 업/다운로드 어플리케이션 개발을 13년도 <span class='text-info font-italic'>3개월 가량 진행</span>하였습니다. <span class='text-info font-italic'>개발 및 배포를 담당</span>하였고, Objective C가 사용되었습니다. 
                 </p>
             </div>
             <div class='project-content'>
@@ -39,7 +39,7 @@
                     <a target='_blank' href='https://www.u-pds.com/' class='align-text-top badge badge-primary'>link</a>
                 </h5>
                 <p class='w-100 text-left'>
-                    (주)유씨알엠의 고객관리 서비스 u-bis의 유지보수를 13년부터 15년 8월퇴사까지 2년 8개월 가량 진행하였고, u-bis서비스의 개편 사이트인 u-pds의 리뉴얼 담당을 이어받아 리뉴얼을 진행하였습니다. 백엔드에는 ASP.NET 4.0와 MSSQL을 프론트엔드에서는 AngularJS를 메인으로 jQuery등의 라이브러리를 사용하였습니다. 결제 모듈 등의 기능 설치 또한 담당하였습니다. 
+                    (주)유씨알엠의 고객관리 서비스 u-bis의 유지보수를 13년부터 15년 8월퇴사까지 <span class='text-info font-italic'>2년 8개월 가량 진행</span>하였고, u-bis서비스의 개편 사이트인 <span class='text-info font-italic'>u-pds의 리뉴얼 개발 담당</span>을 이어받아 리뉴얼을 진행하였습니다. 백엔드에는 <span class='text-info font-italic'>ASP.NET 4.0와 MSSQL</span>을 프론트엔드에서는 <span class='text-info font-italic'>AngularJS를 메인으로 jQuery등</span>의 라이브러리를 사용하였습니다. 결제 모듈 등의 기능 설치 또한 담당하였습니다. 
                 </p>
             </div>
         </div>
